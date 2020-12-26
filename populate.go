8120b56fd33d24627c9a490e470246b7f0e19299
Aaron Weisberg <aaronweisberg@gmail.com>
2020-12-25 17:01:41 -0700

    adds more type support

--- populate.go
+++ populate.go
@@ -16,11 +16,35 @@ func Populate(cmd *cobra.Command, ptr interface{}) error {
 		arg := f.Tag.Get("arg")
 		if arg != "" {
 			fmt.Printf("arg: %s\n", arg)
-			val, err := cmd.Flags().GetString(arg)
-			if err != nil {
-				return err
+
+			switch f.Type.Kind() {
+			case reflect.String:
+				val, err := cmd.Flags().GetString(arg)
+				if err != nil {
+					return err
+				}
+				reflect.ValueOf(ptr).Elem().Field(i).SetString(val)
+			case reflect.Bool:
+				val, err := cmd.Flags().GetBool(arg)
+				if err != nil {
+					return err
+				}
+				reflect.ValueOf(ptr).Elem().Field(i).SetBool(val)
+			case reflect.Slice:
+				vals, err := cmd.Flags().GetStringArray(arg)
+				if err != nil {
+					return err
+				}
+				reflect.ValueOf(ptr).Elem().Field(i).Set(reflect.ValueOf(vals))
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				val, err := cmd.Flags().GetInt(arg)
+				if err != nil {
+					return err
+				}
+				reflect.ValueOf(ptr).Elem().Field(i).SetInt(int64(val))
+			default:
+				continue
 			}
-			reflect.ValueOf(ptr).Elem().Field(i).SetString(val)
 		}
 	}
 	return nil
--- setflags.go
+++ setflags.go
@@ -3,6 +3,7 @@ package cliconfig
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -12,11 +13,15 @@ import (
 
 // SetFlags registers flags from struct tags.
 // Example: field `arg:"flag-name" required:"false" desc:"description" short:"fm"`
-func SetFlags(cmd *cobra.Command, str interface{}) {
+func SetFlags(cmd *cobra.Command, str interface{}) error {
 	t := reflect.TypeOf(str)
 	for i := 0; i < t.NumField(); i++ {
 		f := t.Field(i)
 		name := f.Tag.Get("arg")
+		// skip fields with missing tags
+		if name == "" {
+			continue
+		}
 		desc := f.Tag.Get("desc")
 		short := f.Tag.Get("short")
 		def := f.Tag.Get("default")
@@ -26,13 +31,25 @@ func SetFlags(cmd *cobra.Command, str interface{}) {
 		switch f.Type.Kind() {
 		case reflect.String:
 			cmd.Flags().StringP(name, short, def, desc)
-			// case reflect.SliceOf(reflect:
-			// 	cmd.Flags().StringArrayP(name, short, []string{}, usage)
+		case reflect.Bool:
+			cmd.Flags().BoolP(name, short, def == "true", desc)
+		case reflect.Slice:
+			defArr := strings.Split(def, ",")
+			cmd.Flags().StringArrayP(name, short, defArr, desc)
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			defInt, err := strconv.Atoi(def)
+			if err != nil {
+				return err
+			}
+			cmd.Flags().IntP(name, short, defInt, desc)
+		default:
+			continue
 		}
 		if required {
 			cmd.MarkFlagRequired(name)
 		}
 	}
+	return nil
 }
 
 // BindViperDefaults each cobra flag to its associated viper configuration (config file and environment variable)
